e6fba3df137365dfbe73f5dabd6e2dca5d262c0c
Ryan Carlos <114867274+Ryan-Carloso@users.noreply.github.com>
2025-05-13 12:31:49 +0100

    1

--- app/(tabs)/PaywallOnBoard.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { withIAPContext } from "react-native-iap";
-import Subscriptions from '../../components/Paywall/PaywallOnboard'
-
-const WrappedSubscriptions = withIAPContext(Subscriptions);
-
-export default function App() {
-  return <WrappedSubscriptions />;
-}
